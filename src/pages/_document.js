703ac88b489cad6bb90ea6df2f23f1e03bea624b
Sam.Donovan <samcdonovan@gmail.com>
2022-11-16 18:12:19 +0000

    Update clouds on home page

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function MyDocument() {
     async function getInitialProps(ctx) {
