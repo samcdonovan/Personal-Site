eaa1ff2dd60bf9f894fc77bde9026c77dc71a9f2
Sam.Donovan <samcdonovan@gmail.com>
2022-11-17 11:02:26 +0000

    Move nav menu to right

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -47,7 +47,7 @@ export default function Navbar() {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
 
               <li className={"nav-item" + (pathname === "/" ? " active" : "")}>
                 <Link className="nav-link" href="/">About</Link>
