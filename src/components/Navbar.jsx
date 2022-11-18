333311486aac115f65faae16138decbb6445e4f5
Sam.Donovan <samcdonovan@gmail.com>
2022-11-18 12:22:32 +0000

    Update various phone site bugs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,8 +42,8 @@ export default function Navbar() {
       <div className="container-fluid">
         <nav className="navbar navbar-expand-lg">
 
-          <ToggleButton className="navbar-brand" />
-          <button className="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <ToggleButton />
+          <button className="hamburger navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
